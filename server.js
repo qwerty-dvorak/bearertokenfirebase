ea3a36250708e031d6c8146d0fe3bce36b67fa98
Adheesh Garg <adheeshgarg0611@gmail.com>
2025-08-12 16:45:34 +0000

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/env', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
